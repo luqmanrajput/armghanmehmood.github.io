6ca7b4924c06ca2447aa298e744aa1e2b63279a1
armghan mehmood <armghannmehmood@gmail.com>
2023-08-06 04:00:16 -0700

    to predeploy

--- src/components/Tech/Tech.js
+++ src/components/Tech/Tech.js
@@ -1,108 +1,83 @@
-import React from 'react'
-import '../Tech/Tech.css'
+import React from "react";
+import "../Tech/Tech.css";
 const Tech = () => {
   return (
     <>
-        <div className="style__div">
-            
-        </div>
-        <div id="tech">
-            <div className="container">
-            <h1 className='tech__title'>Tech. & Expertise</h1>
-            <div className="container__skill">
-                <div className="skill__box">
-                    <div className="skill__title">
-                        <div className="skill__img">
-                            <img src="images/html.svg" className="skill__icon" alt="..." />
-                        </div>
-                        <h3>HTML 5</h3>
-                    </div>
-                    <p>
-                        Intermediate
-                    </p>
+      <div className="style__div"></div>
+      <div id="tech">
+        <div className="container">
+          <h1 className="tech__title">Tech. & Expertise</h1>
+          <div className="container__skill">
+            <div className="skill__box">
+              <div className="skill__title">
+                <div className="skill__img">
+                  <img
+                    src="images/photoshop.svg"
+                    className="skill__icon"
+                    alt="..."
+                  />
                 </div>
-                <div className="skill__box">
-                    <div className="skill__title">
-                        <div className="skill__img">
-                            <img src="images/css.svg" className="skill__icon" alt="..." />
-                        </div>
-                        <h3>CSS</h3>
-                    </div>
-                    <p>
-                        Intermediate
-                    </p>
-                </div>
-                <div className="skill__box">
-                    <div className="skill__title">
-                        <div className="skill__img">
-                            <img src="images/javascript.svg" className="skill__icon" alt="..." />
-                        </div>
-                        <h3>JavaScript</h3>
-                    </div>
-                    <p>
-                        Intermediate
-                    </p>
-                </div>
-                <div className="skill__box">
-                    <div className="skill__title">
-                        <div className="skill__img">
-                            <img src="images/bootstrap.svg" className="skill__icon" alt="..." />
-                        </div>
-                        <h3>Bootstrap</h3>
-                    </div>
-                    <p>
-                        Intermediate
-                    </p>
-                </div>
-                <div className="skill__box">
-                    <div className="skill__title">
-                        <div className="skill__img">
-                            <img src="images/react.svg" className="skill__icon" alt="..." />
-                        </div>
-                        <h3>React.js</h3>
-                    </div>
-                    <p>
-                        Intermediate
-                    </p>
-                </div>
-                <div className="skill__box">
-                    <div className="skill__title">
-                        <div className="skill__img">
-                            <img src="images/mongodb.svg" className="skill__icon" alt="..." />
-                        </div>
-                        <h3>MongoDB</h3>
-                    </div>
-                    <p>
-                       Beginner
-                    </p>
-                </div>
-                <div className="skill__box">
-                    <div className="skill__title">
-                        <div className="skill__img">
-                            <img src="images/express.svg" className="skill__icon" alt="..." />
-                        </div>
-                        <h3>Express.js</h3>
-                    </div>
-                    <p>
-                        Beginner
-                    </p>
-                </div>
-                <div className="skill__box">
-                    <div className="skill__title">
-                        <div className="skill__img">
-                            <img src="images/node.svg" className="skill__icon" alt="..." />
-                        </div>
-                        <h3>Node.js</h3>
-                    </div>
-                    <p>
-                        Beginner
-                    </p>
-                </div>
+                <h3>Adobe Photoshop</h3>
+              </div>
+              <p>Intermediate</p>
             </div>
+            <div className="skill__box">
+              <div className="skill__title">
+                <div className="skill__img">
+                  <img
+                    src="images/premiere.svg"
+                    className="skill__icon"
+                    alt="..."
+                  />
+                </div>
+                <h3>Adobe Premiere Pro</h3>
+              </div>
+              <p>Intermediate</p>
             </div>
+            <div className="skill__box">
+              <div className="skill__title">
+                <div className="skill__img">
+                  <img
+                    src="images/lightroom.svg"
+                    className="skill__icon"
+                    alt="..."
+                  />
+                </div>
+                <h3>Lightroom CC</h3>
+              </div>
+              <p>Intermediate</p>
+            </div>
+            <div className="skill__box">
+              <div className="skill__title">
+                <div className="skill__img">
+                  <img
+                    src="images/snapspeed.svg"
+                    className="skill__icon"
+                    alt="..."
+                  />
+                </div>
+                <h3>Snapseed</h3>
+              </div>
+              <p>Advanced</p>
+            </div>
+            <div className="skill__box">
+              <div className="skill__title">
+                <div className="skill__img">
+                  <img
+                    src="images/resolve.svg"
+                    className="skill__icon"
+                    alt="..."
+                  />
+                </div>
+                <h3>DaVinci Resolve</h3>
+              </div>
+              <p>Beginner</p>
+            </div>
+          </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Tech
+export default Tech;
